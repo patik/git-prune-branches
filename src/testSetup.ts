ce62d03968f9e69a45f6aadd6e2ee6b3aace7e38
Craig Patik <craig@patik.com>
2025-05-24 22:33:44 +0200

    fix tests

--- src/testSetup.ts
+++ src/testSetup.ts
@@ -9,7 +9,6 @@ let tempdir: string = process.argv[3]?.split('=')[1] || ''
 let workingDir: string = ''
 
 export const testSetup = () => {
-    console.log('begin')
     if (isCI) {
         child_process.execSync('git config --global user.email "you@example.com"', { cwd: tempdir })
         child_process.execSync('git config --global user.name "Your Name"', { cwd: tempdir })
@@ -49,10 +48,10 @@ export const testSetup = () => {
     // create branch with renamed name, which is NOT deleted on remote
     child_process.execSync('git branch chore/local-name-persistent', { cwd: workingDir })
     // create new branch, which can be deleted only with -D flag
-    child_process.execSync('git branch not-yet-merged', { cwd: workingDir })
+    child_process.execSync('git branch no-ff', { cwd: workingDir })
 
     // checkout working branch
-    child_process.execSync('git checkout not-yet-merged', { cwd: workingDir })
+    child_process.execSync('git checkout no-ff', { cwd: workingDir })
 
     // update file content
     writeFileSync(file, 'lolipop content changed')
@@ -66,19 +65,16 @@ export const testSetup = () => {
     child_process.execSync('git push origin -u chore/local-name-persistent:chore/remote-name-persistent', {
         cwd: workingDir,
     })
-    child_process.execSync('git push origin -u not-yet-merged', { cwd: workingDir })
+    child_process.execSync('git push origin -u no-ff', { cwd: workingDir })
 
     // remove all the branches from the remote, except for the local-name
     child_process.execSync('git push origin :feature/fast-forwarded', { cwd: workingDir })
-    child_process.execSync('git push origin :not-yet-merged', { cwd: workingDir })
+    child_process.execSync('git push origin :no-ff', { cwd: workingDir })
     child_process.execSync('git push origin :"#333-work"', { cwd: workingDir })
     child_process.execSync('git push origin :chore/remote-name-deleted', { cwd: workingDir })
 
     // checkout main branch
     child_process.execSync('git checkout main', { cwd: workingDir })
 
-    console.log('workingDir: ', workingDir)
     return workingDir
 }
-
-testSetup()
