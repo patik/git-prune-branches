69ca0f1e63690f22bb6c8d9456d646d8534e2a89
Craig Patik <craig@patik.com>
2025-06-01 21:54:11 +0200

    small fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
-        testTimeout: 30000, // For git operations
-        hookTimeout: 30000,
+        testTimeout: 10000,
+        hookTimeout: 10000,
         include: ['src/tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}'],
         exclude: ['src/tests/setup.ts', 'src/tests/manual.ts'],
     },
